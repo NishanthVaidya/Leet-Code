35be4f3063ea557007d6a5486b0d86064dd01638
NishanthVaidya <34414512+NishanthVaidya@users.noreply.github.com>
2025-03-31 10:52:09 +0530

    Create 20 - Valid Parantheses.py

---
+++ 20 - Valid Parantheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pair = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in pair.values():
+                stack.append(char)
+            elif char in pair:
+                if not stack or stack[-1] != pair[char]:
+                    return False
+                stack.pop()
+            else:
+                return False  
+
+        return not stack
