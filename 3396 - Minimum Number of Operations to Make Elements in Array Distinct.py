74c12ed964c4f304dad76c3b7b06712fe81f63e8
NishanthVaidya <34414512+NishanthVaidya@users.noreply.github.com>
2025-04-08 06:18:07 +0530

    Create 3396 - Minimum Number of Operations to Make Elements in Array Distinct.py

---
+++ 3396 - Minimum Number of Operations to Make Elements in Array Distinct.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        seen = set()
+
+        for i in reversed(range(len(nums))):
+            if nums[i] in seen:
+                return i // 3 + 1
+            seen.add(nums[i])
+
+        return 0
